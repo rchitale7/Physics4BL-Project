8c98f409710fca1d9f581c64f99442a31aba0ae5
Rohan Chitale <r.chitale7@gmail.com>
2021-03-01 09:48:15 -0800

    Added code to read files from sound_samples folder

--- note_classification/note_classification_code.py
+++ note_classification/note_classification_code.py
@@ -1,3 +1,25 @@
 # read in sound files
 # perform fourier transform
 # create matrix/table with accuracy of note classification
+
+import os
+from scipy.fftpack import fft
+from scipy import signal
+from scipy.io import wavfile
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+trumpet_path =  '../sound_samples/trumpet'
+cello_path =  '../sound_samples/cello'
+sax_path = '../sound_samples/saxophone'
+
+
+def read_files(path):
+    files = os.listdir(path)
+    for file in files:
+        if file.endswith(".wav"):
+            rate, data = wavfile.read(path + '/' + file)
+            print(rate)
+read_files(trumpet_path)
+
