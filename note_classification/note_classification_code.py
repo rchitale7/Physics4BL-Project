6e640321cccba0aca64e1a99c390e0fae89b48d8
Rohan Chitale <r.chitale7@gmail.com>
2021-03-01 10:54:37 -0800

    Implemented Harmonic Product Spectrum functionality

--- note_classification/note_classification_code.py
+++ note_classification/note_classification_code.py
@@ -10,6 +10,7 @@ from scipy.io import wavfile
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import copy
 
 trumpet_path =  '../sound_samples/trumpet'
 cello_path =  '../sound_samples/cello'
@@ -17,6 +18,8 @@ sax_path = '../sound_samples/saxophone'
 
 CONCERT_PITCH = 440
 ALL_NOTES = ["A","A#","B","C","C#","D","D#","E","F","F#","G","G#"]
+NUM_HPS = 8
+
 def find_closest_note(pitch):
   i = int( np.round( np.log2( pitch/CONCERT_PITCH )*12 ) )
   closestNote = ALL_NOTES[i%12] + str(4 + np.sign(i) * int( (9+abs(i))/12 ) )
@@ -24,27 +27,25 @@ def find_closest_note(pitch):
   return closestNote, closestPitch
 
 def get_peak(rate, data):
+    
     samp_period = 1. / rate
     num_data_points = len(data)
+
+    frequencies = np.linspace(0,1,num_data_points) * rate
+
     fft_out = fft(data)
     abs_fft = np.abs(fft_out)
 
-    # Create an array that goes from 0 to the 44100 Hz
-    frequencies = np.linspace(0,1,num_data_points) * rate
-    plt.plot(frequencies,np.abs(fft_out))
-    plt.xlim([100, 1000])
+    hpsSpec = copy.deepcopy(abs_fft)
+    for i in range(NUM_HPS):
+      tmpHpsSpec = np.multiply(hpsSpec[:int(np.ceil(len(abs_fft)/(i+1)))], abs_fft[::(i+1)])
+      if not any(tmpHpsSpec):
+        break
+      hpsSpec = tmpHpsSpec
 
-    peaks, _ = find_peaks(abs_fft, height= 2700000, distance = 30* num_data_points/rate)
-    peaks = peaks[peaks > 50 * num_data_points/rate] #Makes the minimum frequency for a peak be 200 Hz
+    maxInd = np.argmax(hpsSpec)
 
-    # Find the maximum value of the Fourier transform
-    max_value = np.max(abs_fft)
-
-    # Find the index where the max location is
-    max_location = np.where(abs_fft == max_value)[0][0]
-
-    # Now we find the frequency where this happens
-    peak_location = frequencies[max_location]
+    peak_location = frequencies[maxInd]
     return peak_location
 
 def read_files(path):
@@ -54,6 +55,7 @@ def read_files(path):
             rate, data = wavfile.read(path + '/' + file)
             peak = get_peak(rate, data)
             note, pitch  = find_closest_note(peak)
+            print(pitch)
             real_note = file.split("_")[1]
             print("Predicted: ")
             print(note)
